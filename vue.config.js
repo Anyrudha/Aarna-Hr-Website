de8cf48de2ebc1c23702c8bd0f7c1b2b4c5418ad
Anirudha <kulkarnianirudhap@gmail.com>
2022-08-10 00:15:35 +0530

    Updated vue.config

--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,4 @@ const { routes } = require("./src/router");
 
 module.exports = {
     publicPath: '/Aarna-Hr-Website/'
-        // pluginOptions: {
-        //     sitemap: {
-        //         pretty: true,
-        //         baseURL: "",
-        //         routes,
-        //         urls: [],
-        //         defaults: {
-        //             lastmod: new Date().toISOString().slice(0, 10),
-        //             changefreq: "daily",
-        //             priority: 1.0,
-        //         },
-        //     },
-        // },
 };
